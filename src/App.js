3cb0fa2f8392001ef4a21b63c8a975bc416fc7f0
Enkhtvshn <116425332+Enkhtvshn@users.noreply.github.com>
2023-04-03 14:01:51 +0800

    kk

--- src/App.js
+++ src/App.js
@@ -8,18 +8,28 @@ function App() {
   const getAllBranches = async () => {
     try {
       const res = await axios.get("http://localhost:8010/restaurant");
-      setBranches(res.data.restaurant);
+      setBranches(res.data.restaurants);
     } catch (err) {
       console.log("ERRR", err);
     }
   };
+
+  const getNearBranch = async()=>{
+    try{
+      const res = await axios.post("http://localhost:8010/restaurant/near?distance=200", { lat: 47.92373532844665,
+      lon:106.93413474965705,} );
+      setBranches(res.data.branches)
+    } catch(err){
+      console.log("ERROR", err);
+    }
+  }
 
   return (
     <div className="App">
       <h1>Gazrin zurag</h1>
       <div>
         <button onClick={getAllBranches}>bun salbarig haruulah</button>
-        <button>ugugdsun zaid oirig haruulah</button>
+        <button onClick={getNearBranch} >ugugdsun zaid oirig haruulah</button>
       </div>
       <div style={{ width: "100%", height: "90vh", backgroundColor: "grey" }}>
         <MapContainer
@@ -32,10 +42,13 @@ function App() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {branches  && branches.map((x)=>
+          <Marker position={[47.92373532844665, 106.93413474965705]}>
+            <Popup>Seoul Business Center</Popup>
+          </Marker>
+          {branches.length >0  && branches.map((x)=>
           <Marker position={[x.location.coordinates[1], x.location.coordinates[0]]}>
             <Popup>
-              {x.name}. <br /> Easily customizable.
+              {x.name}
             </Popup>
           </Marker> )}
         </MapContainer>
